c65215ee128993c6a1c7351a457910d4698c876e
Emily Martins <emily.flakeheart@gmail.com>
2019-07-31 22:17:39 +0200

    style: add rustfmt.toml configuration
    This will ensure we have consistent style.

--- src/components/model.rs
+++ src/components/model.rs
@@ -4,6 +4,6 @@ use specs::{Component, VecStorage};
 #[derive(Component, Debug)]
 #[storage(VecStorage)]
 pub struct ModelComponent {
-    pub vao: u32,
-    pub length: i32,
+	pub vao: u32,
+	pub length: i32,
 }

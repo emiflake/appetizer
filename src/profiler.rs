3cac1d69fa64acf331cce0a990d78a56547d345b
JannesD <jannesdrijkoningen@gmail.com>
2019-08-05 23:12:11 +0200

    feat: profiler dynamic view range

--- src/profiler.rs
+++ src/profiler.rs
@@ -2,11 +2,11 @@ use std::collections::VecDeque;
 
 pub struct Profiler {
 	pub delays: VecDeque<f32>,
-	pub view_window: usize,
+	pub view_window: i32,
 }
 
 impl Profiler {
-	pub fn new(view_window: usize) -> Self {
+	pub fn new(view_window: i32) -> Self {
 		Self {
 			delays: VecDeque::new(),
 			view_window,
@@ -14,7 +14,7 @@ impl Profiler {
 	}
 
 	pub fn record_delay(&mut self, delay: f32) {
-		if self.delays.len() > self.view_window {
+		while self.delays.len() as i32 > self.view_window {
 			self.delays.pop_front();
 		}
 		self.delays.push_back(delay);
@@ -25,13 +25,14 @@ impl Profiler {
 		self.delays.iter().copied().collect()
 	}
 
-	pub fn draw_ui(&self, delta_time: f32, ui: &mut imgui::Ui) {
+	pub fn draw_ui(&mut self, delta_time: f32, ui: &mut imgui::Ui) {
 		use imgui::Condition;
 		imgui::Window::new(ui, im_str!("Profiler"))
 			.size([400.0, 125.0], Condition::FirstUseEver)
 			.position([50.0, 200.0], Condition::FirstUseEver)
 			.build(|| {
 				ui.text(format!("FPS: {:.2}/{:.5}ms", 1.0 / delta_time, delta_time));
+				ui.slider_int(im_str!("View window"), &mut self.view_window, 10, 1000).build();
 
 				ui.plot_lines(im_str!("Delay (ms)"), self.as_vec().as_ref())
 					.graph_size([300.0, 75.0])
